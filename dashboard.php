9956fa39c08b4be8b341778c2e24764c2ae90e88
mattana57 <66010914057@msu.ac.th>
2026-08-31 15:54:22 +0700

    a

--- dashboard.php
+++ dashboard.php
@@ -1041,8 +1041,8 @@ $dept_icons = [
                 </div>
 
                 <div>
-                    <h3 class="text-lg md:text-xl font-extrabold text-slate-800 mb-3 flex items-center">Administrators</h3>
-                    <p class="text-sm font-medium text-slate-500 mt-1">Manage administrators</p>
+                    <h3 class="text-lg md:text-xl font-extrabold text-slate-800 flex items-center">Administrators</h3>
+                    <p class="text-sm font-medium text-slate-500 mt-1 mb-5">Manage administrators</p>
                     <div class="modern-card overflow-hidden">
                         <div class="overflow-x-auto w-full pb-4 custom-scrollbar">
                             <table class="w-full text-left whitespace-nowrap min-w-[700px]">
@@ -1110,12 +1110,12 @@ $dept_icons = [
                 </div>
 
                 <div class="mt-16 space-y-6">
-                    <div class="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4">
-                <div>
-                    <h3 class="text-lg md:text-xl font-extrabold text-slate-800 mb-3 flex items-center">Administrators</h3>
-                    <p class="text-sm font-medium text-slate-500 mt-1">Manage administrators</p>
-                    <div class="modern-card overflow-hidden">
-                        <div class="flex flex-col sm:flex-row flex-wrap gap-2.5 items-center w-full sm:w-auto">
+                    <div class="flex flex-col lg:flex-row justify-between items-start lg:items-center gap-4">
+                        <div>
+                            <h3 class="text-lg md:text-xl font-extrabold text-slate-800 flex items-center">Technicians</h3>
+                            <p class="text-sm font-medium text-slate-500 mt-1">Manage technicians</p>
+                        </div>
+                        <div class="flex flex-col sm:flex-row flex-wrap gap-2.5 items-center w-full lg:w-auto">
                             <!-- ช่องค้นหา แบบแยกการทำงานสำหรับหน้า Team -->
                             <div class="relative w-full sm:w-48 lg:w-56 mb-2 sm:mb-0">
                                 <i class="fas fa-search absolute left-3 top-1/2 -translate-y-1/2 text-slate-400 text-sm"></i>
