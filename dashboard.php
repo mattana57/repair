03c6751f8b57dcdca3a1aa73d7d92df1dfb81b3b
mattana57 <66010914057@msu.ac.th>
2026-08-31 14:41:57 +0700

    a

--- dashboard.php
+++ dashboard.php
@@ -1041,7 +1041,7 @@ $dept_icons = [
                 </div>
 
                 <div>
-                    <h3 class="text-base font-extrabold text-slate-700 mb-3 flex items-center">Administrators</h3>
+                    <h3 class="text-lg md:text-xl font-extrabold text-slate-800 mb-3 flex items-center">Administrators</h3>
                     <div class="modern-card overflow-hidden">
                         <div class="overflow-x-auto w-full pb-4 custom-scrollbar">
                             <table class="w-full text-left whitespace-nowrap min-w-[700px]">
@@ -1110,7 +1110,7 @@ $dept_icons = [
 
                 <div class="mt-16 space-y-6">
                     <div class="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4">
-                        <h3 class="text-base font-extrabold text-slate-700 flex items-center">Technicians</h3>
+                        <h3 class="text-lg md:text-xl font-extrabold text-slate-800 flex items-center">Technicians</h3>
                         <div class="flex flex-col sm:flex-row flex-wrap gap-2.5 items-center w-full sm:w-auto">
                             <!-- ช่องค้นหา แบบแยกการทำงานสำหรับหน้า Team -->
                             <div class="relative w-full sm:w-48 lg:w-56 mb-2 sm:mb-0">
