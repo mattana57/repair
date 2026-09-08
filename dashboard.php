6e8c479877642ccbfbc64dfb836c240a620d7c0a
mattana57 <66010914057@msu.ac.th>
2026-09-08 11:05:37 +0700

    a

--- dashboard.php
+++ dashboard.php
@@ -2289,7 +2289,7 @@ $dept_icons = [
             
             <div class="px-5 py-4 border-b border-slate-100 flex flex-col md:flex-row justify-between items-start md:items-center gap-4 bg-slate-50 rounded-t-3xl shrink-0">
                 <p class="text-lg md:text-xl font-extrabold text-slate-800 truncate w-full md:w-auto" id="historyModalTitle">History</p>
-                <div class="flex items-center gap-3 md:gap-4 w-full md:w-auto justify-between md:justify-end">
+                <div class="flex items-center gap-4 md:gap-16 w-full md:w-auto justify-between md:justify-end">
                     
                     <!-- ✨ ช่องค้นหาใน Modal ประวัติ ✨ -->
                     <div class="relative w-full md:w-64 flex-1 md:flex-none">
@@ -2297,7 +2297,7 @@ $dept_icons = [
                         <input type="text" id="searchHistoryModalInput" oninput="searchHistoryModalTable()" placeholder="ค้นหาข้อมูลในตาราง..." class="w-full bg-white border border-slate-200 text-xs md:text-sm rounded-xl pl-9 pr-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-100 transition-all font-medium shadow-sm">
                     </div>
 
-                    <div class="flex items-center gap-4 md:gap-8 shrink-0">
+                    <div class="flex items-center gap-3 shrink-0">
                         <button id="historyModalLinkBtn" class="text-[11px] md:text-sm font-bold text-white bg-indigo-600 hover:bg-indigo-700 px-3 md:px-5 py-2 rounded-xl shadow-md shadow-indigo-200 transition-all flex items-center justify-center cursor-pointer">
                             <i class="fas fa-address-book md:mr-1.5"></i> <span class="hidden md:inline">Contacts</span>
                         </button>
