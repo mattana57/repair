7b28e7fbc81325962aa614dc17826b85c97cce68
mattana57 <66010914057@msu.ac.th>
2026-09-26 19:54:35 +0700

    a

--- db_connect.php
+++ db_connect.php
@@ -1,16 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "Project_02"; 
-$dbname = "repair_db";
+// อันนี้ดึงตัวแปรจากไฟล์ env.php
+require_once 'env.php';
 
-// สร้างการเชื่อมต่อ
-$conn = new mysqli($servername, $username, $password, $dbname);
+// เชื่อมต่อโดยใช้ตัวแปรจาก env.php 
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
-// ตั้งค่าให้รองรับภาษาไทย
+// ตั้งค่าให้รองรับภาษาไทยและ Emoji
 $conn->set_charset("utf8mb4");
 
-// เช็คการเชื่อมต่อ
+// อันนี้เช็คการเชื่อมต่อ
 if ($conn->connect_error) {
   die("เชื่อมต่อฐานข้อมูลล้มเหลว: " . $conn->connect_error);
 }
